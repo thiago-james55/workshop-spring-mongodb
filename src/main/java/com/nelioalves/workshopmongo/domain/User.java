6b17d768abeca2b4fcadbf8aa9a49b4070f89605
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-08-27 18:53:37 -0300

    Creating entity post with nested user

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Document(collection = "user")
+@Document
 public class User implements Serializable {
 
     @Id
