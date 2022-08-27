7f112ade172908f164e4689c9283ce0f6ef8cf1e
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-08-27 17:38:16 -0300

    Delete

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -54,5 +54,14 @@ public class UserResource {
 
     }
 
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
 
 }
