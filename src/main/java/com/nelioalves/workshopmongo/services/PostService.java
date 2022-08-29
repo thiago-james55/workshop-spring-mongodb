880a9824e98c015acb4b91a502d292bc05c2ddda
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-08-29 19:59:37 -0300

    Endpoint to get post by id

---
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.nelioalves.workshopmongo.services;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public List<Post> findAll() {
+        return repository.findAll();
+    }
+
+    public Post findById(String id) {
+
+        Optional<Post> Post = repository.findById(id);
+
+        if ( Post.isPresent() ) { return Post.get(); }
+        else { throw new ObjectNotFoundException("Post not found! ID: " + id); }
+
+    }
+
+}
