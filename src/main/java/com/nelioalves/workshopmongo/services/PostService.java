9175d78f0f0eed6cebea6ffeef925e2533dc90d0
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-08-30 19:56:08 -0300

    Search from @Query

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -29,7 +29,7 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title) {
-        return repository.findByTitleContainingIgnoreCase(title);
+        return repository.findByTitle(title);
     }
 
 }
